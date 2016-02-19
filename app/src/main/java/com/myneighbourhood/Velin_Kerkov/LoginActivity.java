e23e6ee728124c81245fc9b7df32e4cb3c0186e6
Velin Kerkov <v.b.k.2007@abv.bg>
2016-02-19 16:47:41 +0000

    LoginActivity finished

--- app/src/main/java/com/myneighbourhood/Velin_Kerkov/LoginActivity.java
+++ app/src/main/java/com/myneighbourhood/Velin_Kerkov/LoginActivity.java
@@ -1,14 +1,47 @@
 package com.myneighbourhood.Velin_Kerkov;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.myneighbourhood.Kiril_Hristov.MainActivity;
 import com.myneighbourhood.R;
 
 public class LoginActivity extends BaseActivity {
+
+    private EditText passwordET;
+    private EditText usernameET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // bind to UI elements
+        passwordET = (EditText) findViewById(R.id.login_ET_password);
+        usernameET = (EditText) findViewById(R.id.login_ET_username);
+        Button loginBTN = (Button) findViewById(R.id.login_B_login);
+        Button registerBTN = (Button) findViewById(R.id.login_B_register);
+
+        // set up listeners
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = passwordET.getText().toString();
+                String username = usernameET.getText().toString();
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
