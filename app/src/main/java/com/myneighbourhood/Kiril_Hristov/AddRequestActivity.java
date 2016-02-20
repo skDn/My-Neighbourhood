12f444241587e054dc6e4cdb5ae0a3d7940f7200
Kiril Hristov <kirilhristov91@gmail.com>
2016-02-20 03:05:17 +0000

    number picker for people implemented

--- app/src/main/java/com/myneighbourhood/Kiril_Hristov/AddRequestActivity.java
+++ app/src/main/java/com/myneighbourhood/Kiril_Hristov/AddRequestActivity.java
@@ -1,6 +1,7 @@
 package com.myneighbourhood.Kiril_Hristov;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,18 +14,22 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 
+
 import com.myneighbourhood.R;
 import com.myneighbourhood.Velin_Kerkov.BaseActivity;
 import com.myneighbourhood.Velin_Kerkov.MainActivity;
 
-public class AddRequestActivity extends BaseActivity implements View.OnClickListener{
+public class AddRequestActivity extends BaseActivity implements NumberPicker.OnValueChangeListener{
 
     LinearLayout addRequestLayout;
     EditText titleField;
     EditText descriptionField;
-    NumberPicker peoplePicker;
     DatePicker datePicker;
     Button postButton;
+    Button selectNumberPeople;
+    Button selectDate;
+
+    private int numberOfPeopleSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -34,9 +39,36 @@ public class AddRequestActivity extends BaseActivity implements View.OnClickList
 
         titleField = (EditText) findViewById(R.id.AddRequestTitle);
         descriptionField = (EditText) findViewById(R.id.AddRequestDescription);
-        peoplePicker = (NumberPicker) findViewById(R.id.AddRequestNumberPickerPeople);
+
         datePicker = (DatePicker) findViewById(R.id.AddRequestDatePicker);
         postButton = (Button) findViewById(R.id.AddRequestPostButton);
+        selectNumberPeople = (Button) findViewById(R.id.selectNumberPeopleButton);
+        selectDate = (Button) findViewById(R.id.selectDateButton);
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("VLIZAM TUKA");
+                String title = titleField.getText().toString();
+                String description = descriptionField.getText().toString();
+                if (title.equals("")) showError();
+            }
+        });
+
+        numberOfPeopleSelected = 1;
+        selectNumberPeople.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNumberPicker();
+            }
+        });
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         // hide keyboard on ACTION_DOWN
         addRequestLayout = (LinearLayout) findViewById(R.id.AddRequestLinearLayout);
@@ -47,6 +79,32 @@ public class AddRequestActivity extends BaseActivity implements View.OnClickList
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+        numberOfPeopleSelected = newVal;
+        System.out.println(numberOfPeopleSelected);
+    }
+
+    public void showNumberPicker() {
+        final Dialog d = new Dialog(AddRequestActivity.this);
+        d.setTitle("How many people do you need?");
+        d.setContentView(R.layout.dialog_number_picker);
+        Button done = (Button) d.findViewById(R.id.NumberPickerButton);
+        final NumberPicker peoplePicker = (NumberPicker) d.findViewById(R.id.NumberPickerPeople);
+        peoplePicker.setMaxValue(10);
+        peoplePicker.setMinValue(1);
+        peoplePicker.setWrapSelectorWheel(false);
+        peoplePicker.setOnValueChangedListener(this);
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                d.dismiss();
+            }
+        });
+        d.show();
     }
 
     @Override
@@ -61,23 +119,6 @@ public class AddRequestActivity extends BaseActivity implements View.OnClickList
     private void hideKeyboard(View view) {
         InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         in.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.AddRequestPostButton:
-
-                String title = titleField.getText().toString();
-                String description = descriptionField.getText().toString();
-                int peopleNeeded = peoplePicker.getValue();
-                System.out.println(peopleNeeded);
-                if(title.equals("")) showError();
-                else{
-
-                }
-                break;
-        }
     }
 
     private void showError(){
