9c2e39e47cdd5f4f4f3912d2c704c3838136b9d6
skDn <2038011y@student.gla.ac.uk>
2016-02-23 02:42:31 +0200

    Addded News reference class

---
+++ app/src/main/java/com/myneighbourhood/utils/News.java
@@ -0,0 +1,70 @@
+package com.myneighbourhood.utils;
+
+/**
+ * Created by skDn on 23/02/2016.
+ */
+public class News {
+    private int newsId;
+    private int userId;
+    private String title;
+    private String text;
+    private String timestamp;
+//    private String picture;
+
+    public News(int newsId, int userId, String title, String text, String timestamp) {
+        this.newsId = newsId;
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+        this.timestamp = timestamp;
+//        this.picture = picture;
+    }
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+//    public String getPicture() {
+//        return picture;
+//    }
+//
+//    public void setPicture(String picture) {
+//        this.picture = picture;
+//    }
+}
