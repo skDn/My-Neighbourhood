8d5274ec69e8daf26f32ebe7a5f7702181744b70
Velin Kerkov <v.b.k.2007@abv.bg>
2016-02-25 15:30:13 +0000

    Profile connected

--- app/src/main/java/com/myneighbourhood/Velin_Kerkov/ProfileActivity.java
+++ app/src/main/java/com/myneighbourhood/Velin_Kerkov/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.myneighbourhood.R;
 import com.myneighbourhood.utils.Utils;
@@ -16,6 +17,22 @@ public class ProfileActivity extends BaseActivity {
         setContentView(R.layout.activity_profile);
 
         Button logOffBTN = (Button) findViewById(R.id.profile_B_log_off);
+        TextView ratingRequesterTV = (TextView) findViewById(R.id.profile_TV_rating_requester);
+        TextView ratingApplicantTV = (TextView) findViewById(R.id.profile_TV_rating_applicant);
+        TextView ratingEndorsedByTV = (TextView) findViewById(R.id.profile_TV_rating_endorsed_by);
+        TextView usernameTV = (TextView) findViewById(R.id.profile_TV_username);
+        TextView firstNameTV = (TextView) findViewById(R.id.profile_TV_first_name);
+        TextView lastNameTV = (TextView) findViewById(R.id.profile_TV_last_name);
+        TextView emailTV = (TextView) findViewById(R.id.profile_TV_email);
+        TextView phoneTV = (TextView) findViewById(R.id.profile_TV_phone);
+        TextView addressTV = (TextView) findViewById(R.id.profile_TV_address);
+
+        usernameTV.setText(user.getUsername());
+        emailTV.setText(user.getEmail());
+        phoneTV.setText(user.getPhone());
+
+
+
         logOffBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
